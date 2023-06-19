8c8936548af04f13e601db5fed2359b6769a8bad
fidelicura <fidelicura@gmail.com>
2023-06-19 09:40:17 +0300

    removed python-dotenv

--- src/general.py
+++ src/general.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta
 from logging import logging, INFO
+from os import getenv
 
 
 
 configure_logging(logging, INFO)
 
-
 def read_keys():
-    API_KEY = 
-    ADD_KEY = 
+    API_KEY = getenv("API_KEY")
+    ADD_KEY = getenv("")
 
 def calculate_time_delta():
     time_format = "%Y.%m.%d"
